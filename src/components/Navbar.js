e707e1edd13aeee90f874ca222a281ec00e9df76
James <jamesrobertsongroves@gmail.com>
2023-04-27 14:01:15 +0100

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+const Navbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="" component="div" sx={{ flexGrow: 1 }}>
+          Bank of React
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
